28790f51d4ae1a8a5f4577dde120d136857e20d0
Brandon Juarez Ponce <brandonjuarez@wolfsellers.com>
2024-02-04 00:20:24 -0600

    Brjupo_CustomerAddress - Create Custom Customer Address Attribute Programatically 100% Working on Adobe Commerce Enterprise Edition v2.4.5-p1

---
+++ Brjupo/CustomerAddress/Model/CreateCustomerAddressAttribute.php
@@ -0,0 +1,310 @@
+<?php
+
+namespace Brjupo\CustomerAddress\Model;
+
+use Magento\Customer\Api\Data\AddressInterface;
+use Magento\Customer\Model\AttributeFactory;
+use Magento\CustomerCustomAttributes\Helper\Address as HelperAddress;
+use Magento\CustomerCustomAttributes\Helper\Data as HelperData;
+use Magento\Eav\Api\Data\AttributeInterface;
+use Magento\Eav\Model\Config;
+use Magento\Eav\Model\Entity\Attribute\SetFactory;
+use Magento\Eav\Model\Entity\Type as EavEntityType;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Filter\FilterManager;
+use Magento\Framework\Registry;
+use Magento\Framework\Serialize\Serializer\FormData;
+use Magento\Store\Model\WebsiteFactory;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * This class will work as main functionality to create Custom Customer Address Attributes programatically
+ * Taking native class as example, this class is found when we save an attribute manually in Admin
+ * This execute method now will return a void or exception INSTEAD of redirect as native class
+ * Also will receive $data as params INSTEAD of a request->getPostValue()
+ *
+ * Code copied from
+ * module-customer-custom-attributes/Controller/Adminhtml/Customer/Address/Attribute/Save.php
+ */
+class CreateCustomerAddressAttribute
+{
+    /**
+     * Customer Address Entity Type instance
+     *
+     * @var EavEntityType
+     */
+    protected $_entityType;
+
+    /**
+     * @var HelperData
+     */
+    protected $helperData;
+
+    /**
+     * @var HelperAddress
+     */
+    protected $helperAddress;
+
+    /**
+     * @var FilterManager
+     */
+    protected $filterManager;
+
+    protected LoggerInterface $logger;
+
+    /**
+     * @var FormData|null
+     */
+    private $formDataSerializer;
+
+    /**
+     * @var array
+     */
+    private $deniedAttributes = [];
+
+    /**
+     * @param Registry $coreRegistry
+     * @param Config $eavConfig
+     * @param AttributeFactory $attrFactory
+     * @param SetFactory $attrSetFactory
+     * @param WebsiteFactory $websiteFactory
+     * @param HelperData $helperData
+     * @param HelperAddress $helperAddress
+     * @param FilterManager $filterManager
+     * @param LoggerInterface $logger
+     * @param FormData|null $formDataSerializer
+     * @param array $deniedAttributes
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        Registry         $coreRegistry,
+        Config           $eavConfig,
+        AttributeFactory $attrFactory,
+        SetFactory       $attrSetFactory,
+        WebsiteFactory   $websiteFactory,
+        HelperData       $helperData,
+        HelperAddress    $helperAddress,
+        FilterManager    $filterManager,
+        LoggerInterface  $logger,
+        FormData         $formDataSerializer = null,
+        array            $deniedAttributes = []
+    )
+    {
+        $this->_coreRegistry = $coreRegistry;
+        $this->_eavConfig = $eavConfig;
+        $this->_attrFactory = $attrFactory;
+        $this->_attrSetFactory = $attrSetFactory;
+        $this->websiteFactory = $websiteFactory;
+
+        $this->helperData = $helperData;
+        $this->helperAddress = $helperAddress;
+        $this->filterManager = $filterManager;
+
+        $this->logger = $logger;
+
+        $this->formDataSerializer = $formDataSerializer ?: ObjectManager::getInstance()->get(FormData::class);
+        $this->deniedAttributes = $deniedAttributes;
+    }
+
+    /**
+     * Save attribute action
+     *
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @throws LocalizedException
+     */
+    public function execute($data)
+    {
+        // TODO: Add validation in case the attribute exists!
+        // TODO: Remember you delete, on february 3rd, an IF that considered the case the attribute exists
+        // TODO: Reconsidered it, in case a developer wants to update attribute programatically
+        if (!$data) {
+            throw new LocalizedException(__('Data is NOT provided'));
+            return;
+        }
+
+        try {
+            if (!isset($data['serialized_options'])) {
+                $data['serialized_options'] = '[]';
+            }
+            $optionData = $this->formDataSerializer->unserialize($data['serialized_options']);
+        } catch (\InvalidArgumentException $e) {
+            throw new LocalizedException(__("The attribute couldn't be saved due to an error. Verify your information and try again. "
+                . "If the error persists, please try again later. " . $e->getMessage()));
+            return;
+        }
+
+        $data = array_replace_recursive(
+            $data,
+            $optionData
+        );
+
+        /* @var $attributeObject \Magento\Customer\Model\Attribute */
+        $attributeObject = $this->_initAttribute();
+
+        //filtering
+        try {
+            $data = $this->helperAddress->filterPostData($data);
+        } catch (LocalizedException $e) {
+            throw new LocalizedException($e->getMessage());
+            return;
+        }
+
+
+        $data['backend_model'] = $this->helperData->getAttributeBackendModelByInputType(
+            $data['frontend_input']
+        );
+        $data['source_model'] = $this->helperData->getAttributeSourceModelByInputType($data['frontend_input']);
+        $data['backend_type'] = $this->helperData->getAttributeBackendTypeByInputType($data['frontend_input']);
+        $data['is_user_defined'] = 1;
+        $data['is_system'] = 0;
+
+        // add set and group info
+        $data['attribute_set_id'] = $this->_getEntityType()->getDefaultAttributeSetId();
+        /** @var $attrSet \Magento\Eav\Model\Entity\Attribute\Set */
+        $attrSet = $this->_attrSetFactory->create();
+        $data['attribute_group_id'] = $attrSet->getDefaultGroupId($data['attribute_set_id']);
+
+
+        //Always assign the default form
+        if (!isset($data['used_in_forms_disabled'])) {
+            $defaultFormName = 'adminhtml_customer_address';
+            if (empty($data['used_in_forms'])) {
+                $data['used_in_forms'] = [$defaultFormName];
+            } elseif (is_array($data['used_in_forms'])) {
+                $data['used_in_forms'][] = $defaultFormName;
+            }
+        }
+
+        $defaultValueField = $this->helperData->getAttributeDefaultValueByInput($data['frontend_input']);
+        if ($defaultValueField) {
+            $scopeKeyPrefix = '';
+            if(isset($data['website']) && $data['website']){
+                $scopeKeyPrefix = 'scope_';
+            }
+            if(isset($data[$scopeKeyPrefix . $defaultValueField])){
+                $defaultValue = $data[$scopeKeyPrefix . $defaultValueField];
+                $data[$scopeKeyPrefix . 'default_value'] = $defaultValue
+                    ? $this->filterManager->stripTags($defaultValue) : null;
+            }
+            else {
+                $data[$scopeKeyPrefix . 'default_value'] = null;
+            }
+        }
+
+        $data['entity_type_id'] = $this->_getEntityType()->getId();
+        $data['validate_rules'] = $this->helperData->getAttributeValidateRules($data['frontend_input'], $data);
+
+        $validateRulesErrors = $this->helperData->checkValidateRules(
+            $data['frontend_input'],
+            $data['validate_rules']
+        );
+        if (count($validateRulesErrors)) {
+            $allErrors = '';
+            foreach ($validateRulesErrors as $message) {
+                $allErrors = $allErrors . ' ' . $message;
+            }
+            throw new LocalizedException($allErrors);
+            return;
+        }
+
+        $attributeObject->addData($data);
+
+        /**
+         * Check "Use Default Value" checkboxes values
+         */
+        if(isset($data['use_default'])){
+            $useDefaults = $data['use_default'];
+            if ($useDefaults) {
+                foreach ($useDefaults as $key) {
+                    if (!is_array($key) && !in_array($key, $this->deniedAttributes)) {
+                        $attributeObject->setData($key, null);
+                    }
+                }
+            }
+        }
+
+        try {
+            $frontendLabel = $attributeObject->getDataByKey(AttributeInterface::FRONTEND_LABEL);
+            $attributeObject->save();
+
+            if ($this->isRegionAttribute($attributeObject)) {
+                $this->setRegionIdFrontendLabel($frontendLabel);
+            }
+            $this->logger->info(__('You saved the customer address attribute.'));
+            return;
+        } catch (LocalizedException $e) {
+            throw new LocalizedException($e->getMessage());
+            return;
+        } catch (\Exception $e) {
+            throw new LocalizedException($e->getMessage());
+            return;
+        }
+    }
+
+    /**
+     * Sets RegionId frontend label equal to Region frontend label.
+     *
+     * RegionId is hidden frontend input attribute and isn't available for updating via admin panel,
+     * but frontend label of this attribute is visible in address forms as Region label.
+     * So frontend label for RegionId should be synced with frontend label for Region attribute, which is
+     * available for updating.
+     *
+     * @param array|string $frontendLabel
+     * return void
+     * @throws LocalizedException
+     */
+    private function setRegionIdFrontendLabel($frontendLabel): void
+    {
+        $attributeRegionId = $this->_initAttribute();
+        $attributeRegionId->loadByCode($this->_getEntityType(), AddressInterface::REGION_ID);
+        if ($attributeRegionId->getId()) {
+            $attributeRegionId->setData(AttributeInterface::FRONTEND_LABEL, $frontendLabel);
+            $attributeRegionId->save();
+        }
+    }
+
+    /**
+     * Check if attribute is a region.
+     *
+     * @param \Magento\Customer\Model\Attribute $attributeObject
+     * @return bool
+     */
+    private function isRegionAttribute(\Magento\Customer\Model\Attribute $attributeObject): bool
+    {
+        return $attributeObject->getAttributeCode() === AddressInterface::REGION;
+    }
+
+    /**
+     * Retrieve customer attribute object
+     * Function copied from module-customer-custom-attributes/Controller/Adminhtml/Customer/Address/Attribute.php
+     *
+     * @return \Magento\Customer\Model\Attribute
+     */
+    private function _initAttribute($dataWebsite = null)
+    {
+        /** @var $attribute \Magento\Customer\Model\Attribute */
+        $attribute = $this->_attrFactory->create();
+        $website = $dataWebsite ?: $this->websiteFactory->create();
+        $attribute->setWebsite($website);
+        return $attribute;
+    }
+
+    /**
+     * Return Customer Address Entity Type instance
+     * Function copied from module-customer-custom-attributes/Controller/Adminhtml/Customer/Address/Attribute.php
+     *
+     * @return EavEntityType
+     */
+    private function _getEntityType()
+    {
+        if ($this->_entityType === null) {
+            $this->_entityType = $this->_eavConfig->getEntityType('customer_address');
+        }
+        return $this->_entityType;
+    }
+}
---
+++ Brjupo/CustomerAddress/Setup/Patch/Data/AttributeText.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Brjupo\CustomerAddress\Setup\Patch\Data;
+
+/**
+ * Adobe Commerce Docs - Default dependencies for Data Patch
+ * https://developer.adobe.com/commerce/php/development/components/declarative-schema/patches/
+ */
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+
+/**
+ * Additional dependencies for this Data Patch
+ */
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Model\Indexer\Address\AttributeProvider;
+
+use Brjupo\CustomerAddress\Model\CreateCustomerAddressAttribute;
+
+class AttributeText implements DataPatchInterface, PatchRevertableInterface
+{
+    const ATTRIBUTE_CODE = 'text_2330';
+    const SORT_ORDER = 20000;
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    private CreateCustomerAddressAttribute $createCustomerAddressAttribute;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory     $customerSetupFactory,
+        CreateCustomerAddressAttribute $createCustomerAddressAttribute
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->createCustomerAddressAttribute = $createCustomerAddressAttribute;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws LocalizedException
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $textCustomCustomerAddressAttributeData = [];
+        $textCustomCustomerAddressAttributeData['frontend_label'][0] = 'Feb03_Text_2330';
+        $textCustomCustomerAddressAttributeData['attribute_code'] = self::ATTRIBUTE_CODE;
+        $textCustomCustomerAddressAttributeData['frontend_input'] = 'text';
+        $textCustomCustomerAddressAttributeData['is_required'] = false;
+        $textCustomCustomerAddressAttributeData['default_value_text'] = '';
+
+        $textCustomCustomerAddressAttributeData['input_validation'] = '';
+        $textCustomCustomerAddressAttributeData['min_text_length'] = '';
+        $textCustomCustomerAddressAttributeData['max_text_length'] = '';
+
+        $textCustomCustomerAddressAttributeData['input_filter'] = '';
+        $textCustomCustomerAddressAttributeData['is_used_in_grid'] = false;
+        $textCustomCustomerAddressAttributeData['is_visible_in_grid'] = false;
+        $textCustomCustomerAddressAttributeData['is_searchable_in_grid'] = false;
+        $textCustomCustomerAddressAttributeData['grid_filter_condition_type'] = '0';
+        $textCustomCustomerAddressAttributeData['is_used_for_customer_segment'] = false;
+        $textCustomCustomerAddressAttributeData['is_visible'] = true;
+        $textCustomCustomerAddressAttributeData['sort_order'] = self::SORT_ORDER;
+        $textCustomCustomerAddressAttributeData['used_in_forms'] = [
+            'customer_register_address',
+            'customer_address_edit'
+        ];
+
+        $textCustomCustomerAddressAttributeData['frontend_label'][1] = ''; //Default Store view
+
+        $this->createCustomerAddressAttribute->execute($textCustomCustomerAddressAttributeData);
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * Adobe Commerce and Magento Open Source DO NOT ALLOW YOU TO REVERT A PARTICULAR MODULE DATA PATCH. However, you can revert all composer installed or non-composer installed data patches using the module:uninstall command.
+     * bin/magento module:uninstall --non-composer Vendor_ModuleName
+     * bin/magento module:uninstall Vendor_ModuleName
+     * https://developer.adobe.com/commerce/php/development/components/declarative-schema/patches/#reverting-data-patches
+     *
+     * @return void
+     */
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        //Here should go code that will revert all operations from `apply` method
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $customerSetup->removeAttribute(AttributeProvider::ENTITY, self::ATTRIBUTE_CODE);
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
